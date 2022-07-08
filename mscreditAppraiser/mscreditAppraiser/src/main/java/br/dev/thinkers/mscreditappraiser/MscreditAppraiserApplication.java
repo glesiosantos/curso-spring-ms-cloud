ad94308d242d5f3b47cebecca56004ca093fd52e
Glêsio Santos <glesioss@gmail.com>
2022-07-08 05:01:17 -0300

    add ms credit appraiser

---
+++ mscreditAppraiser/mscreditAppraiser/src/main/java/br/dev/thinkers/mscreditappraiser/MscreditAppraiserApplication.java
@@ -0,0 +1,13 @@
+package br.dev.thinkers.mscreditappraiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscreditAppraiserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscreditAppraiserApplication.class, args);
+    }
+
+}
---
+++ mscreditAppraiser/mscreditAppraiser/src/test/java/br/dev/thinkers/mscreditappraiser/MscreditAppraiserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.thinkers.mscreditappraiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscreditAppraiserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
